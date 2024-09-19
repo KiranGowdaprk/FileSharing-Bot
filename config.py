3ca8bb7cff52f12aca1b5fb2135123e1cfef7c6a
Yash <titanmattersbot05@gmail.com>
2024-09-19 15:03:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,10 +62,9 @@ else:
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
-"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(5356695781)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
