21542587130ad7a9f9dc30e56f062a57aa8bae8a
Yash <titanmattersbot05@gmail.com>
2024-09-26 13:35:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import *
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT
+
 
 name ="""
 ████████╗██╗████████╗░█████╗░███╗░░██╗██╗░░██╗██████╗░░█████╗░████████╗░██████╗
