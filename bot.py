111a73d3527dcefb142382f52babb33169d01484
Yash <titanmattersbot05@gmail.com>
2024-09-30 13:47:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,7 +106,8 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/TitanXBots")
-        self.LOGGER(__name__).info(f""" \n\n       
+        self.LOGGER(__name__).info(f""" \n\n
+        
 ████████╗██╗████████╗░█████╗░███╗░░██╗██╗░░██╗██████╗░░█████╗░████████╗░██████╗
 ╚══██╔══╝██║╚══██╔══╝██╔══██╗████╗░██║╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝██╔════╝
 ░░░██║░░░██║░░░██║░░░███████║██╔██╗██║░╚███╔╝░██████╦╝██║░░██║░░░██║░░░╚█████╗░
