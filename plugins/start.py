9d7be79a3430cf8013f96b03f70c76e3a609b3c1
Yash <titanmattersbot05@gmail.com>
2024-10-02 21:56:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,7 +93,8 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
+        await message.reply_photo(
+            photo= START_PIC,
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
