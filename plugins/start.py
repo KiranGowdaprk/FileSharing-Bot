6173892874c44ba55f57e86c941d440a643f6b09
Yash <titanmattersbot05@gmail.com>
2024-10-02 22:06:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/TitansXY/13')
+                    InlineKeyboardButton("🔰 ʜᴇʟᴘ", callback_data = "help"),
+                    InlineKeyboardButton("⚡ ᴀʙᴏᴜᴛ", callback_data = "about")
                 ]
             ]
         )
