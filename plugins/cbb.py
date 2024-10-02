473f8479fcf104e9c32d2d12ba23d31c15eb7ded
Yash <titanmattersbot05@gmail.com>
2024-10-02 23:46:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,13 +2,27 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
+from config import *
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.database import add_user, del_user, full_userbase, present_user
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data = "start"),
+                    InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
         await query.message.edit_text(
             text=ABOUT_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview = True,
